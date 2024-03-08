1a8bf82cfaede46cdf61e847fcdf6395fff6a9f7
Andrii <andriy30534>
2024-03-08 02:19:05 +0200

    feat: added swagger

---
+++ server/src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const configService = app.get<ConfigService>(ConfigService);
+  const port = configService.get<number>('API_PORT');
+
+  const config = new DocumentBuilder()
+    .setTitle('Todo List API')
+    .setDescription('API for managing todo list')
+    .setVersion('1.0')
+    .addTag('todos')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(port || 3000, () => {
+    console.log(`started on ${port}`);
+  });
+}
+bootstrap();
