fdd25cfd93fef427dfd7598bace1f02eb9c5ffc9
Andrii <andriy30534>
2024-03-09 20:09:18 +0200

    redundant variables removed

--- client/src/components/TodoItem.tsx
+++ client/src/components/TodoItem.tsx
@@ -9,19 +9,15 @@ import {
 } from '@mui/material';
 import { Todo } from '../types/todo';
 import { useStatusTodo } from '../hooks/useTodosQuery';
-import ToDoSnackbar from './SneckBar';
-import { useState } from 'react';
+
 import RemoveButton from './RemoveButton';
 
 export const TodoItem = ({ status, id, todo }: Todo) => {
-  const [open, setOpen] = useState(false);
-
-  const { updateStatus, isSuccess } = useStatusTodo();
+  const { updateStatus } = useStatusTodo();
 
   const handelComplied = () => {
     if (status === 'done') return;
     updateStatus({ id, status: 'done' });
-    setOpen(true);
   };
   const handelProgress = () => {
     if (status === 'progress') return;
@@ -29,50 +25,45 @@ export const TodoItem = ({ status, id, todo }: Todo) => {
   };
 
   return (
-    <>
-      <Grow key={id} in={true}>
-        <ListItem disableGutters>
-          <Paper sx={{ width: '100%', px: { xs: '8px', lg: '16px' } }}>
-            <Stack
-              sx={{ minHeight: '70px' }}
-              spacing={4}
-              direction='row'
-              alignItems={'center'}>
-              <ButtonGroup size='small'>
-                <Button
-                  sx={{ fontSize: '8px' }}
-                  disabled={status === 'progress'}
-                  onClick={handelProgress}
-                  variant='outlined'>
-                  progress
-                </Button>
-                <Button
-                  sx={{ fontSize: { xs: '8px', md: '16' } }}
-                  color='success'
-                  disabled={status === 'done'}
-                  onClick={handelComplied}
-                  variant='outlined'>
-                  done
-                </Button>
-              </ButtonGroup>
-              <Typography
-                sx={{
-                  fontSize: { sx: '16px', md: '24px' },
-                  flexGrow: 1,
-                  textDecoration: status === 'done' ? 'line-through' : 'none',
-                }}
-                component={'p'}>
-                {todo}
-              </Typography>
-              <Typography variant='caption'>{status}</Typography>
-              <RemoveButton id={id} />
-            </Stack>
-          </Paper>
-        </ListItem>
-      </Grow>
-      {open && isSuccess && (
-        <ToDoSnackbar massage={'task successfully completed'} />
-      )}
-    </>
+    <Grow key={id} in={true}>
+      <ListItem disableGutters>
+        <Paper sx={{ width: '100%', px: { xs: '8px', lg: '16px' } }}>
+          <Stack
+            sx={{ minHeight: '70px' }}
+            spacing={4}
+            direction='row'
+            alignItems={'center'}>
+            <ButtonGroup size='small'>
+              <Button
+                sx={{ fontSize: '8px' }}
+                disabled={status === 'progress'}
+                onClick={handelProgress}
+                variant='outlined'>
+                progress
+              </Button>
+              <Button
+                sx={{ fontSize: { xs: '8px', md: '16' } }}
+                color='success'
+                disabled={status === 'done'}
+                onClick={handelComplied}
+                variant='outlined'>
+                done
+              </Button>
+            </ButtonGroup>
+            <Typography
+              sx={{
+                fontSize: { sx: '16px', md: '24px' },
+                flexGrow: 1,
+                textDecoration: status === 'done' ? 'line-through' : 'none',
+              }}
+              component={'p'}>
+              {todo}
+            </Typography>
+            <Typography variant='caption'>{status}</Typography>
+            <RemoveButton id={id} />
+          </Stack>
+        </Paper>
+      </ListItem>
+    </Grow>
   );
 };
